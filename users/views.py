3c26b8af789be966380762799c795dedc28ac43b
Naveen-Ijeri <naveen.ijeri123@gmail.com>
2025-11-09 07:07:48 +0530

    Added users app and first API endpoint

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def hello_user(request):
+    return Response({"message": "Welcome to Django CI/CD Demo!"})
